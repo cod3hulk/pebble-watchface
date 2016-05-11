431d87a93539958e1346caec60f1b9502f31e86f
Tomas Ave <cod3hulk@gmail.com>
2016-05-11 08:48:26 +0200

    Added handlers for handling phone events

--- src/watchface.c
+++ src/watchface.c
@@ -67,6 +67,22 @@ static void main_window_unload(Window *window) {
     fonts_unload_custom_font(s_weather_font);
 }
 
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+    APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
 static void init() {
     s_main_window = window_create();
 
@@ -80,6 +96,12 @@ static void init() {
     update_time();
 
     tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+
+    // register calllbacks
+    app_message_register_inbox_received(inbox_received_callback);
+    app_message_register_inbox_dropped(inbox_dropped_callback);
+    app_message_register_outbox_failed(outbox_failed_callback);
+    app_message_register_outbox_sent(outbox_sent_callback);
 }
 
 static void deinit() {
