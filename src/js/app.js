8277e992b4e856cddcf5483ed86d401129378b43
Tomas Ave <cod3hulk@gmail.com>
2016-05-18 08:28:34 +0200

    Added support for requesting open weather data

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+
 // Listen for when the watchface is opened
 Pebble.addEventListener('ready',
     function (e) {
@@ -5,8 +6,36 @@ Pebble.addEventListener('ready',
     }
 );
 
+var xhrRequest = function (url, type, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    callback(this.responseText);
+  };
+  xhr.open(type, url);
+  xhr.send();
+};
+
 function locationSuccess(pos) {
-  // We will request the weather here
+  console.log('locationSuccess called!');
+  var myAPIKey = '';
+  var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' +
+      pos.coords.latitude + '&lon=' + pos.coords.longitude + '&appid=' + myAPIKey;
+
+  // Send request to OpenWeatherMap
+  xhrRequest(url, 'GET', 
+    function(responseText) {
+      // responseText contains a JSON object with weather info
+      var json = JSON.parse(responseText);
+
+      // Temperature in Kelvin requires adjustment
+      var temperature = Math.round(json.main.temp - 273.15);
+      console.log('Temperature is ' + temperature);
+
+      // Conditions
+      var conditions = json.weather[0].main;      
+      console.log('Conditions are ' + conditions);
+    }      
+  );
 }
 
 function locationError(err) {
