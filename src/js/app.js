e3c2bbf130ce8d6e14334913706addf774296abe
Tomas Ave <cod3hulk@gmail.com>
2016-05-22 15:25:36 +0200

    Fixed formatting

--- src/js/app.js
+++ src/js/app.js
@@ -26,37 +26,37 @@ function locationSuccess(pos) {
   console.log('locationSuccess called!');
   var myAPIKey = '';
   var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' +
-      pos.coords.latitude + '&lon=' + pos.coords.longitude + '&appid=' + myAPIKey;
+    pos.coords.latitude + '&lon=' + pos.coords.longitude + '&appid=' + myAPIKey;
 
   // Send request to OpenWeatherMap
-  xhrRequest(url, 'GET', 
-    function(responseText) {
-      // responseText contains a JSON object with weather info
-      var json = JSON.parse(responseText);
+  xhrRequest(url, 'GET',
+      function(responseText) {
+        // responseText contains a JSON object with weather info
+        var json = JSON.parse(responseText);
 
-      // Temperature in Kelvin requires adjustment
-      var temperature = Math.round(json.main.temp - 273.15);
-      console.log('Temperature is ' + temperature);
+        // Temperature in Kelvin requires adjustment
+        var temperature = Math.round(json.main.temp - 273.15);
+        console.log('Temperature is ' + temperature);
 
-      // Conditions
-      var conditions = json.weather[0].main;      
-      console.log('Conditions are ' + conditions);
+        // Conditions
+        var conditions = json.weather[0].main;
+        console.log('Conditions are ' + conditions);
 
-      var dictionary = {
-		'KEY_TEMPERATURE': temperature,
-		'KEY_CONDITIONS': conditions
-	  };
+        var dictionary = {
+          'KEY_TEMPERATURE': temperature,
+          'KEY_CONDITIONS': conditions
+        };
 
-	  Pebble.sendAppMessage(dictionary,
-		  function (e) {
-			console.log('Weather info sent to Pebble succesfully!')
-		  },
-		  function (e) {
-			console.log('Error sending weather info to Pebble!')
-		  }
-	  );
+        Pebble.sendAppMessage(dictionary,
+            function (e) {
+              console.log('Weather info sent to Pebble succesfully!')
+            },
+            function (e) {
+              console.log('Error sending weather info to Pebble!')
+            }
+        );
 
-    }      
+      }
   );
 }
 
@@ -66,9 +66,9 @@ function locationError(err) {
 
 function getWeather() {
   navigator.geolocation.getCurrentPosition(
-    locationSuccess,
-    locationError,
-    {timeout: 15000, maximumAge: 60000}
+      locationSuccess,
+      locationError,
+      {timeout: 15000, maximumAge: 60000}
   );
 }
 
